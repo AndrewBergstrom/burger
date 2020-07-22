dbbd482a799653588737baed94f7e5c72c856cad
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-07-22 09:16:53 -0700

    burger form now appearing

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,7 +3,7 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get("/", function (req,res){
-    burger.selectAll(function(data){
+    burger.all(function(data){
         var hbsObject = {
             burgers: data
         };
@@ -37,5 +37,18 @@ router.put("/api/burgers/:id", function(req,res){
         }
     });
 });
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 module.exports = router;
