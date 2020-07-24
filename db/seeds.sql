e929f2136289cbc7421f8f612d305fb653ffa919
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-07-23 18:59:49 -0700

    functionally working, just need to add styling

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
